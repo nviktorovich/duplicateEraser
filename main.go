b9a19d378650a02724e676043fd869b8752d3166
NViktorovich <nvmaslenko@gmail.com>
2022-05-11 21:30:58 +0500

    Добавлен валидатор названий файлов. Если файл не существует, возвращается false, существует - true. Добавил цветовую индикацию, для более дружелюбного съема информации. Использовал пакет "github.com/fatih/color"

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/nviktorovich/duplicateEraser/DriverAndSettings"
 	"github.com/nviktorovich/duplicateEraser/InputSettings"
 	"log"
+	"path/filepath"
 )
 
 func main() {
@@ -12,5 +15,23 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(val)
+	isValid, err := DriverAndSettings.FileNameValidate(val)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	path, err := filepath.Abs(val)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("file name is: ", color.GreenString(val))
+	if isValid {
+		fmt.Println("file name is valid: ", color.GreenString("существует"))
+		fmt.Println("full path is : ", color.GreenString(path))
+
+	} else {
+		fmt.Println("file name is valid: ", color.RedString("не существует"))
+	}
+
 }
