7e40e681224eae6ce3b1b77c5692a468cdce4057
NViktorovich <nvmaslenko@gmail.com>
2022-05-18 22:47:08 +0500

    созданы функции Filter и PutData, которые отвечают за фильтрацию мапы, и за ее первичное наполнение данными.

--- LogicProcessors/WalkProcessor.go
+++ LogicProcessors/WalkProcessor.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/fs"
 	"path/filepath"
+	"strconv"
 )
 
 // Walker интерфейс, реализующий метод walk (подготовка для создания заглушки, для метода filepath.Walk()
@@ -29,8 +30,41 @@ func init() {
 	WalkVar = WalkStruct{}
 }
 
+type Banker interface {
+	Filter()
+	PutData(name, path string, size int64)
+}
+
+type FileBank struct {
+	Bank map[string][]string
+}
+
+func NewFileBank() *FileBank {
+	return new(FileBank)
+}
+
+// Filter метод, который фильтрует значения в словаре (тип []string) и удаляет
+// записи, в которых только один экземплряр
+func (f *FileBank) Filter() {
+	for k, v := range f.Bank {
+		if len(v) < 2 {
+			delete(f.Bank, k)
+		}
+	}
+}
+
+// PutData метод для объекта *FileBank, добавляет пару ключ - значение. Ключем
+// является объединенная строка (название_размер), значением - список путей к
+// файлу
+func (f *FileBank) PutData(name, path string, size int64) {
+	strSize := strconv.FormatInt(size, 10)
+	key := name + "_" + strSize
+	f.Bank[key] = append(f.Bank[key], path)
+}
+
 // GetFileList обрабатывает рекурсивно указанную директорию.
 func GetFileList(path string) error {
+	// необходимо настроить работу функции GetFileList таким образом, чтобы получить объект FileBank с заполненной мапой
 	err := WalkVar.walk(path,
 		func(path string, info fs.FileInfo, err error) error {
 			if !info.IsDir() {
